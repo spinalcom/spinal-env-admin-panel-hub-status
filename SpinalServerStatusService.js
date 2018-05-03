b6e141d12fcb627427adc70e905f52bfe0e2d378
laurent <htd.laurent@gmail.com>
2018-05-03 12:50:08 +0200

    update with require d3 angular in src

--- SpinalServerStatusService.js
+++ SpinalServerStatusService.js
@@ -1,27 +1,34 @@
-angular.module('app.spinal-panel')
-  .factory("SpinalServerStatusService", ['authService', '$q', "ngSpinalCore",
-    function (authService, $q, ngSpinalCore) {
-      let spinhubService = {};
+var angular = require("angular");
 
-      spinhubService.init = () => {
-        if (!spinhubService.statusModel) {
-          return authService.wait_connect().then(() => {
-            return ngSpinalCore.load("/etc/Status").then(function (statusModel) {
+angular.module("app.spinal-panel").factory("SpinalServerStatusService", [
+  "authService",
+  "$q",
+  "ngSpinalCore",
+  function(authService, $q, ngSpinalCore) {
+    let spinhubService = {};
+
+    spinhubService.init = () => {
+      if (!spinhubService.statusModel) {
+        return authService.wait_connect().then(
+          () => {
+            return ngSpinalCore.load("/etc/Status").then(function(statusModel) {
               spinhubService.statusModel = statusModel;
-              return (spinhubService.statusModel);
+              return spinhubService.statusModel;
             });
-          }, () => {
+          },
+          () => {
             let msg = "not able to load : /etc/Status";
             console.error(msg);
             deferred.reject(msg);
-          });
-        } else {
-          var deferred = $q.defer();
-          deferred.resolve(spinhubService.statusModel);
-          return deferred.promise;
-        }
-      };
+          }
+        );
+      } else {
+        var deferred = $q.defer();
+        deferred.resolve(spinhubService.statusModel);
+        return deferred.promise;
+      }
+    };
 
-      return spinhubService;
-    }
-  ]);
+    return spinhubService;
+  }
+]);
--- main.js
+++ main.js
@@ -1,41 +1,50 @@
 require("./SpinalServerStatusService");
 require("./ServerStatusCtrl");
-(function () {
-  angular.module('app.spinal-panel')
-    .run(["$templateCache", "$http", "goldenLayoutService",
-      function ($templateCache, $http, goldenLayoutService) {
-        let load_template = (uri, name) => {
-          $http.get(uri).then((response) => {
+var angular = require("angular");
+
+(function() {
+  angular.module("app.spinal-panel").run([
+    "$templateCache",
+    "$http",
+    "goldenLayoutService",
+    function($templateCache, $http, goldenLayoutService) {
+      let load_template = (uri, name) => {
+        $http.get(uri).then(
+          response => {
             $templateCache.put(name, response.data);
-          }, (errorResponse) => {
-            console.log('Cannot load the file ' + uri);
-          });
-        };
-        let toload = [{
-          uri: '../templates/spinal-env-admin-panel-hub-status/hub-status-panel.html',
-          name: 'hub-status-panel.html'
-        }];
-        for (var i = 0; i < toload.length; i++) {
-          load_template(toload[i].uri, toload[i].name);
-        }
-
-        goldenLayoutService.registerPanel({
-          id: "drag-hub-status-panel",
-          name: "SpinalHub Status",
-          cfg: {
-            isClosable: true,
-            title: "SpinalHub Status",
-            type: 'component',
-            width: 20,
-            componentName: 'SpinalHome',
-            componentState: {
-              template: 'hub-status-panel.html',
-              module: 'app.spinal-panel',
-              controller: 'ServerStatusCtrl'
-            }
+          },
+          () => {
+            console.log("Cannot load the file " + uri);
           }
-        });
+        );
+      };
+      let toload = [
+        {
+          uri:
+            "../templates/spinal-env-admin-panel-hub-status/hub-status-panel.html",
+          name: "hub-status-panel.html"
+        }
+      ];
+      for (var i = 0; i < toload.length; i++) {
+        load_template(toload[i].uri, toload[i].name);
       }
-    ]);
 
+      goldenLayoutService.registerPanel({
+        id: "drag-hub-status-panel",
+        name: "SpinalHub Status",
+        cfg: {
+          isClosable: true,
+          title: "SpinalHub Status",
+          type: "component",
+          width: 20,
+          componentName: "SpinalHome",
+          componentState: {
+            template: "hub-status-panel.html",
+            module: "app.spinal-panel",
+            controller: "ServerStatusCtrl"
+          }
+        }
+      });
+    }
+  ]);
 })();
